f02ffa8e059f8cc083187900e3ea5a6514c93bb6
chainick-byte <igorrepyakh@gmail.com>
2020-09-03 23:24:24 +0100

    annado todo lo demas

---
+++ src/main/java/mx/com/probando_git/ProbandoGitApplication.java
@@ -0,0 +1,13 @@
+package mx.com.probando_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbandoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbandoGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/probando_git/ProbandoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.probando_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbandoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
