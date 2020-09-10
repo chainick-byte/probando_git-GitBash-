4d229c6d42b4865c5714e8c7c1a26c807735e5ef
chainick-byte <igorrepyakh@gmail.com>
2020-09-10 17:52:10 +0100

    rama alternativa del proyecto original fast_forward

---
+++ src/main/java/mx/com/probando_git/Fast_Forward.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.probando_git;
+
+/**
+ *
+ * @author igorr
+ * 
+ * con esta clase quiero probar primer tipo de union de la rama principal con la alternativa
+ */
+public class Fast_Forward {
+    
+}
